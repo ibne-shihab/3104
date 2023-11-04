5a8f5c0583412c2962f22c953de9d388960e6016
MD. IBNE SHIHAB SHAD <88297125+ibne-shihab@users.noreply.github.com>
2023-11-04 07:27:36 +0600

    Delete 06.1 forloop.js

--- 06.1 forloop.js
+++
@@ -1,5 +0,0 @@
-for (let i = 0; i <= 30; i++) {
-  const message = i % 2 === 0 ? `${i} is even` : `${i} is odd`;
-  const outputDiv = document.getElementById('output');
-  outputDiv.innerHTML += message + "<br>";
-}
